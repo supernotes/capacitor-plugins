ce1fb874612e2b86de388128f2dbe6826871b0e9
Connor <connor.cn@gmail.com>
2025-04-09 17:30:58 +0700

    feat(clipboard): Upgrade to Capacitor 7

--- clipboard/Package.swift
+++ clipboard/Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "SupernotesCapacitorClipboard",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v14)],
     products: [
         .library(
             targets: ["ClipboardPlugin"])
